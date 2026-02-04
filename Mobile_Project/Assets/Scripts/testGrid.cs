034b8ce22f7f2cc275f0543fb463504cdfbc9c93
RaphaelBattistel <raphaelbattistel@gmail.com>
2026-02-04 16:29:19 +0100

    Rule Tiles + Drag&Drop + Centrage objet dans grille

--- Mobile_Project/Assets/Scripts/testGrid.cs
+++ Mobile_Project/Assets/Scripts/testGrid.cs
@@ -1,11 +1,18 @@
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.PlayerSettings;
 
 public class testGrid : MonoBehaviour
 {
     [SerializeField] private Grid grid;
     [SerializeField] private Camera camera;
+    [SerializeField] private LayerMask objectMask;
+
+    private GameObject selectedObject;
+    private bool isDragging;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,39 +22,99 @@ public class testGrid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DragAndDrop();
+        }
+
+        if (isDragging)
+        {
+            if(selectedObject.TryGetComponent(out Collider2D objectCollider))
+            {
+                objectCollider.isTrigger = true;
+            }
+            if(selectedObject.TryGetComponent(out Rigidbody2D objectRB2D))
+            {
+                objectRB2D.simulated = false;
+            }
+            Vector3 pos = mousePos();
+            //selectedObject.transform.position = pos;
+            selectedObject.transform.position = FindCellCenter();
+        }
+
         if (Input.GetMouseButtonUp(0))
         {
-            Vector3 mouseScreenPosition = Input.mousePosition;
-            mouseScreenPosition.z = Mathf.Abs(camera.transform.position.z);
-            Vector2 mouseWorldPosition = camera.ScreenToWorldPoint(mouseScreenPosition);
-
-            Vector2 mouseCellPlacement = new Vector2();
-
-            switch (mouseWorldPosition.x)
+            if (selectedObject.TryGetComponent(out Collider2D objectCollider))
             {
-                case < 0:
-                    mouseCellPlacement.x = (grid.cellSize.x * (int)(mouseWorldPosition.x / grid.cellSize.x)) - grid.cellSize.x;
-                    break;
-                default:
-                    mouseCellPlacement.x = grid.cellSize.x * (int)(mouseWorldPosition.x/ grid.cellSize.x);
-                    break;
+                objectCollider.isTrigger = false;
             }
-            switch (mouseWorldPosition.y)
+            if (selectedObject.TryGetComponent(out Rigidbody2D objectRB2D))
             {
-                case < 0:
-                    mouseCellPlacement.y = (grid.cellSize.y * (int)(mouseWorldPosition.y / grid.cellSize.y)) - grid.cellSize.y;
-                    break;
-                default:
-                    mouseCellPlacement.y = grid.cellSize.y * (int)(mouseWorldPosition.y/ grid.cellSize.y);
-                    break;
+                objectRB2D.simulated = true;
             }
-
-            Vector2 mouseCellCenter = new Vector2(mouseCellPlacement.x + (grid.cellSize.x / 2), mouseCellPlacement.y + (grid.cellSize.y / 2));
-
-            Debug.DrawLine(mouseCellPlacement, new Vector2(mouseCellPlacement.x + grid.cellSize.x, mouseCellPlacement.y), Color.red, 1.0f);
-            Debug.DrawLine(mouseCellPlacement, new Vector2(mouseCellPlacement.x, mouseCellPlacement.y + grid.cellSize.y), Color.red, 1.0f);
-            Debug.DrawLine(mouseCellPlacement, mouseCellCenter, Color.forestGreen, 1.0f);
-            Debug.Log(mouseCellPlacement);
+            isDragging = false;
+            //selectedObject.transform.position = FindCellCenter();
         }
     }
+
+    private Vector2 FindCellCenter()
+    {
+        Vector2 mouseCellPlacement = new Vector2();
+
+        switch (mousePos().x)
+        {
+            case < 0:
+                mouseCellPlacement.x = (grid.cellSize.x * (int)(mousePos().x / grid.cellSize.x)) - grid.cellSize.x;
+                break;
+            default:
+                mouseCellPlacement.x = grid.cellSize.x * (int)(mousePos().x / grid.cellSize.x);
+                break;
+        }
+        switch (mousePos().y)
+        {
+            case < 0:
+                mouseCellPlacement.y = (grid.cellSize.y * (int)(mousePos().y / grid.cellSize.y)) - grid.cellSize.y;
+                break;
+            default:
+                mouseCellPlacement.y = grid.cellSize.y * (int)(mousePos().y / grid.cellSize.y);
+                break;
+        }
+
+        Vector2 mouseCellCenter = new Vector2(mouseCellPlacement.x + (grid.cellSize.x / 2), mouseCellPlacement.y + (grid.cellSize.y / 2));
+
+        //Debug.DrawLine(mouseCellPlacement, new Vector2(mouseCellPlacement.x + grid.cellSize.x, mouseCellPlacement.y), Color.red, 1.0f);
+        //Debug.DrawLine(mouseCellPlacement, new Vector2(mouseCellPlacement.x, mouseCellPlacement.y + grid.cellSize.y), Color.red, 1.0f);
+        //Debug.DrawLine(mouseCellPlacement, mouseCellCenter, Color.forestGreen, 1.0f);
+
+        return mouseCellCenter;
+    }
+
+    private void DragAndDrop()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+
+        Ray ray = camera.ScreenPointToRay(mousePosition);
+
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 1000, objectMask);
+        if (hit.collider != null)
+        {
+            Debug.Log("Hit!");
+            selectedObject = hit.collider.gameObject;
+            isDragging = true;
+        }
+        else
+        {
+            Debug.Log("HELL NAH");
+        }
+    }
+
+    Vector2 mousePos()
+    {
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        mouseScreenPosition.z = Mathf.Abs(camera.transform.position.z);
+        Vector2 mouseWorldPosition = camera.ScreenToWorldPoint(mouseScreenPosition);
+
+        return mouseWorldPosition;
+    }
+
 }
