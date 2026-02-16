5d7963585b623cad2e32cad0639bd2e46b728431
Malukker <arthur.beauquesne@gmail.com>
2026-02-16 17:34:15 +0100

    [MOD] Level buttons spawning in menu

---
+++ Mobile_Project/Assets/Scripts/System/LevelsMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class LevelsMenuManager : MonoBehaviour
+{
+    public static LevelsMenuManager Instance;
+    
+    [SerializeField, BoxGroup("References")] private Transform _buttonOrganiser;
+    
+    [SerializeField, BoxGroup("Data")] private LevelDatabase _levelDatabase;
+    
+    [SerializeField, BoxGroup("Objects")] private LevelSelectionButton _buttonPrefab;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+        
+        SpawnButtonsFromDatabase();
+    }
+
+    private void SpawnButtonsFromDatabase()
+    {
+        foreach (LevelData levelData in _levelDatabase.Levels)
+        {
+            Instantiate(_buttonPrefab, _buttonOrganiser).SetLevel(levelData.LevelId, levelData.Level.name);
+        }
+    }
+}
--- Mobile_Project/Assets/Scripts/UI/LevelSelectionButton.cs
+++ Mobile_Project/Assets/Scripts/UI/LevelSelectionButton.cs
@@ -1,9 +1,17 @@
+using TMPro;
 using UnityEngine;
 
 public class LevelSelectionButton : MonoBehaviour
 {
-    [SerializeField] private int _levelId;
+    [SerializeField, HideInInspector] private int _levelId;
+    [SerializeField] private TextMeshProUGUI _nameText;
 
+    public void SetLevel(int id, string label)
+    {
+        _levelId = id;
+        _nameText.text = label;
+    }
+    
     public void LoadLevelScene()
     {
         //LevelManager.Instance.LoadLevel(_levelId, "Level");
