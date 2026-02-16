15cf457cb088d092ef2213f2eecebfae1d46cdde
Malukker <arthur.beauquesne@gmail.com>
2026-02-16 15:58:29 +0100

    [MOD] Google Play Games setup

--- Mobile_Project/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Mobile_Project/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -39,7 +39,7 @@ namespace GooglePlayGames {
         public const string ApplicationId = "15200980578"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
         public const string WebClientId = ""; // Filled in automatically
-        public const string NearbyConnectionServiceId = "";
+        public const string NearbyConnectionServiceId = "com.LateFish.Elementalia";
 
         public static bool ApplicationIdInitialized() {
             return !string.IsNullOrEmpty(ApplicationId) && !ApplicationId.Equals(ToEscapedToken(UnescapedApplicationId));
