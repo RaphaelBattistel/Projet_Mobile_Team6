638767f4fba71119ce5dac041ea91c6825ca7692
RaphaelBattistel <raphaelbattistel@gmail.com>
2026-02-17 16:58:44 +0100

    Fix Codex + UI Codex in ScenePrincipale

--- Mobile_Project/Assets/Scripts/UI/CodexImage.cs
+++ Mobile_Project/Assets/Scripts/UI/CodexImage.cs
@@ -9,20 +9,28 @@ public class CodexImage : MonoBehaviour
     [SerializeField] Image Item1Image;
     [SerializeField] Image Item2Image;
 
-    private void Start()
+    private Sprite _original;
+
+    private void Awake()
     {
         FusionManager.Instance.OnFusionItem.AddListener(ChangeCodexImage);
+        _original = fusionImage.sprite;
     }
 
     public void ChangeCodexImage(ItemData item)
     {
-        if (item == itemFusion && fusionImage.sprite == null)
+        if (item == itemFusion && fusionImage.sprite == _original)
         {
             fusionImage.sprite = item.Sprite;
             FusionDatabase.FusionRecipe recipe = FusionManager.Instance.Database.GetRecipe(item);
             Item1Image.sprite = recipe.ElementA.Sprite;
             Item2Image.sprite = recipe.ElementB.Sprite;
         }
+    }
+
+    private void OnDestroy()
+    {
+        FusionManager.Instance.OnFusionItem.RemoveListener(ChangeCodexImage);
     }
 
 }
