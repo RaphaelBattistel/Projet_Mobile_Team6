31b2f588914569288bfee79c900e6eaa06cb6376
Malukker <arthur.beauquesne@gmail.com>
2026-02-17 10:04:17 +0100

    [MOD] Gameloop WIP

--- Mobile_Project/Assets/Scripts/System/LevelManager.cs
+++ Mobile_Project/Assets/Scripts/System/LevelManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,7 +11,14 @@ public class LevelManager : MonoBehaviour
 
     public LevelData CurrentLevel { get; private set; }
 
-    void Awake()
+    private AsyncOperation _loadingOperation;
+    
+    private Animator _loadingScreenAnimator;
+
+    private static readonly int LoadingStart = Animator.StringToHash("LoadingStart");
+    private static readonly int LoadingDone = Animator.StringToHash("LoadingDone");
+
+    private void Awake()
     {
         if (Instance != null)
         {
@@ -21,21 +30,68 @@ public class LevelManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    public void LoadLevel(int levelId, string sceneName)
+    public void LoadLevel(int levelId)
     {
         CurrentLevel = _levelDatabase.GetLevel(levelId);
-        
+
         if (CurrentLevel == null)
         {
             Debug.LogError($"Level {levelId} not found");
             return;
         }
 
-        SceneManager.LoadScene(sceneName);
+        // Ca devrait charger sauf si un petit malin décide de faire n'importe quoi avec les build settings
+        _loadingOperation = SceneManager.LoadSceneAsync(1);
+        if (_loadingOperation is null)
+        {
+            throw new UnassignedReferenceException("Scene 1 not found in build settings");
+        }
+
+        _loadingOperation.allowSceneActivation = false;
+        StartCoroutine(AnimateLevelLoading());
+    }
+    
+    private IEnumerator AnimateLevelLoading()
+    {
+        // On laisse tourner l'anim tant que la scène charge, puis on lui indique de s'achever
+        _loadingScreenAnimator.SetTrigger(LoadingStart);
+        
+        while (!_loadingOperation.isDone)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        _loadingScreenAnimator.SetTrigger(LoadingDone);
     }
 
-    public void UnloadCurrentLevel()
+    public void EndLevelLoading()
     {
-        
+        // On appelle ça dans une fonction grâce à une notify dans l'animation de fin déclenchée plus haut
+        _loadingOperation.allowSceneActivation = true;
+    }
+
+    public async void UnloadCurrentLevel()
+    {
+        try
+        {
+            // Pareil, normalement la scène devrait se charger sauf dans le cas d'un sabotage
+            _loadingOperation = SceneManager.LoadSceneAsync(0);
+            if (_loadingOperation is null)
+            {
+                throw new UnassignedReferenceException("Scene 0 not found in build settings");
+            }
+
+            _loadingOperation.allowSceneActivation = false;
+            StartCoroutine(AnimateLevelLoading());
+
+            // On attend que le chargement soit achevé pour faire le ménage
+            await _loadingOperation;
+            
+            CurrentLevel = null;
+        }
+        catch
+        {
+            throw new Exception("Either something went wrong or the playmode was ended while loading");
+        }
     }
 }
